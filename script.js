cd365950018c6fed5eac38f05375e1f4e27e0088
MildaPau <pauzaite.milda@gmail.com>
2022-02-02 22:32:02 +0200

    main section tabs

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
 "use strict";
 
+// tabs
 
 
+function changeContent(index, id){
+    document.querySelector(".gellary").style.paddingLeft = "0";
+    var articles = document.querySelectorAll('.articles');
+    
+    var tabs = document.querySelectorAll('.square-img');
+    var element = document.getElementById(id);
+    
+    for (let i = 0; i < articles.length; i++){
+        articles[i].style.display = "none";
+        tabs[i].style.backgroundColor = "#A37169";
+    }
 
+    element.style.display = "block";
+    element.style.opacity = "1";
+    tabs[index].style.backgroundColor = "#E27866";
+
+
+}
+
+    // var article = document.getElementById('#pirmas');
+    // var sections = document.querySelectorAll("section");
+    // var buttons = document.querySelectorAll("button");
+
+
+    // for (let i = 0; i < articles.length; i++){
+    //     articles[i].classList.remove("display");
+    //     // pClick[i].classList.remove("active");
+    // }
+
+
+    // var element = document.getElementById(id);
+    // element.classList.add("display");
+    // buttons[index].classList.add("active");
+
+    // setTimeout(function(){
+    //     sections[index].style.opacity = 0.5;
+    // }, 100);
+    // setTimeout(function(){
+    //     sections[index].style.opacity = 1;
+    // }, 200);
 
