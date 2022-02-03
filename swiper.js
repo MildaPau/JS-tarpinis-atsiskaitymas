25dc0ac73cd677e8b923282e10539440da98e797
MildaPau <pauzaite.milda@gmail.com>
2022-02-03 15:54:57 +0200

    section pricing-options swiper

--- swiper.js
+++ swiper.js
@@ -1,7 +1,44 @@
 "use strict"
 
+//Section - pricing-options
+const swiper2 = new Swiper('#pricing-options .swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 1,
+    loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+      
+      1100: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        allowSlideNext: false,
+        allowSlidePrev: false,
+      },
+      900: {
+        slidesPerView: 2,
+        slidesPerGroup: 1,
+        allowSlideNext: true,
+        allowSlidePrev: true,
+      },
+  }
+
+});
+
+
 //Section - costumers-saying
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('#costumers-saying .swiper', {
     // Optional parameters
     direction: 'horizontal',
     slidesPerView: 1,
