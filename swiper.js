f82d0bfe56643fcb6d88d7e8a587acb11e52bf27
MildaPau <pauzaite.milda@gmail.com>
2022-02-03 13:12:23 +0200

    pricing-options - slides effect

---
+++ swiper.js
@@ -0,0 +1,38 @@
+"use strict"
+
+// alert("veikia");
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 1,
+    loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+      
+      1100: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        allowSlideNext: false,
+        allowSlidePrev: false,
+      },
+      900: {
+        slidesPerView: 2,
+        slidesPerGroup: 1,
+        allowSlideNext: true,
+        allowSlidePrev: true,
+      },
+  }
+
+});
