0d93a0fca5984b9a5d1ec8e9137230bf06e1c3f8
MildaPau <pauzaite.milda@gmail.com>
2022-02-04 11:40:19 +0200

    Responsive navigation hover

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // responsive nav bar
 var burger = document.getElementById("burger");
 var anchors = document.querySelectorAll(".nav-a");
-
+var aHovers = document.querySelectorAll(".nav-a-hover");
 
 burger.addEventListener("click", function(){
     for(var x of anchors){
@@ -15,18 +15,23 @@ burger.addEventListener("click", function(){
 
 
 
-// for (var x of anchors){
-//     x.addEventListener("click", function(){
-//         // this.style.color = "red";
-//         for(var i = 0; i < anchors.length; i++){
-//             anchors[i].style.color = "white";
-//         }
-//         this.style.color = "red";
-//     });
-// }
+for (var x of anchors){
+    x.addEventListener("click", function(){
+        // this.style.color = "red";
+        for(var i = 0; i < anchors.length; i++){
+            anchors[i].style.color = "#FFFFFF";
+            anchors[i].style.fontWeight = "400";
+            anchors[i].style.fontSize = "14";
 
-
-
+        }
+        this.style.color = "#E27866";
+        this.style.fontWeight = "800";
+        this.style.fontSize = "20";
+    });
+    // anchors[5].addEventListener("click", function(){
+    // anchors.style.color = "#FFFFFF";
+    // })
+}
 
 
 // tabs
